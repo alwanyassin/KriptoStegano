d132d4ecdce094a1ce87806d5b41240eb17890e4
Alwan Yassin <43112324+alwanyassin@users.noreply.github.com>
2019-12-17 20:57:48 +0700

    Add files via upload

---
+++ tutorial.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>
+			Tutorial Aplikasi
+		</title>
+	</head>
+	<body>
+		<h1>Tutorial Penggunaan Aplikasi (Work in Progress)</h1>
+		Video Encode<br>
+		Video Decode
+	</body>
+</html>
